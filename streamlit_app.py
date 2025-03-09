bc6e0a542526aea7998cbb0d827bfe777cbf870d
yusa-san <82858638+yusa-san@users.noreply.github.com>
2025-03-09 17:23:40 +0000

    Updated app selected files listing

--- streamlit_app.py
+++ streamlit_app.py
@@ -316,23 +316,21 @@ def file_selection_screen():
     # 選択されたファイルの一覧
     st.write("### 選択されたファイル一覧:")
     print(st.session_state)
+    if "folder_entries" in st.session_state and st.session_state["folder_entries"]: # Inputフォルダからのファイル情報
+        st.write("#### Inputフォルダ:")
+        for file_info in st.session_state["folder_entries"]:
+            st.write(f"{file_info.get('name', 'error:name')} ({file_info.get('source', 'error:source')})")
+            st.write(f"file_info: {file_info}")
     if "url_entries" in st.session_state and st.session_state["url_entries"]: # URL入力によるファイル情報
         st.write("#### URL入力:")
         for file_info in st.session_state["url_entries"]:
-            st.write(f"{file_info['name']} ({file_info['source']})")
+            st.write(f"{file_info.get('url', 'error:url')} ({file_info.get('source', 'error:source')})")
             st.write(f"file_info: {file_info}")
-    elif "folder_entries" in st.session_state and st.session_state["folder_entries"]: # Inputフォルダからのファイル情報
-        st.write("#### Inputフォルダ:")
-        for file_info in st.session_state["folder_entries"]:
-            st.write(f"{file_info['name']} ({file_info['source']})")
-            st.write(f"file_info: {file_info}")
-    elif "upload_entries" in st.session_state and st.session_state["upload_entries"]:  #アップロードによるファイル情報
+    if "upload_entries" in st.session_state and st.session_state["upload_entries"]:  #アップロードによるファイル情報
         st.write("#### アップロード:")
         for file_info in st.session_state["upload_entries"]:
-            st.write(f"{file_info['name']} ({file_info['source']})")
+            st.write(f"{file_info.get('name', 'error:name')} ({file_info.get('source', 'error:source')})")
             st.write(f"file_info: {file_info}")
-    else:
-        st.write("ファイルが選択されていません。")
 
 def display_dashboard(selected_files):
     st.header("ダッシュボード表示画面")
