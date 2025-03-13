3b9a056db3b9b0ed6436aa2b23c1d206c47db1d8
yusa-san <82858638+yusa-san@users.noreply.github.com>
2025-03-13 03:49:00 +0000

    Updated reverse refactor and cache

--- streamlit_app.py
+++ streamlit_app.py
@@ -483,7 +483,7 @@ def display_dashboard():
                         get_color_expr = [200,30,0,160]
                     # サイズ
                     radius = st.sidebar.text_input(f"半径", value=10, key=f"radius_key_{file_name}")
-                    # アイコンで表示かポイントで表示かを選択
+                    # アイコン表示かポイント表示かを選択
                     # if st.sidebar.checkbox("アイコンで表示", value=False):
                     #     # アイコンのアトラス（1枚の画像に複数のアイコンが含まれる画像）と、アイコンのマッピング情報を設定
                     #     icon_atlas = "./resource/icon-atlas.png"
